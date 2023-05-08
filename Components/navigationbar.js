99232bd545490f242efe7d86ef81d499a848f594
LukoshkinAnarchy <54411516+LukoshkinAnarchy@users.noreply.github.com>
2023-05-08 15:47:30 +0300

    commit msg

--- Components/navigationbar.js
+++ Components/navigationbar.js
@@ -1,12 +1,73 @@
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { motion } from "framer-motion";
+import Web3 from "web3";
 
-const CustomLink = ({ href, children }) => {
-  return <div onClick={() => (window.location.href = href)}>{children}</div>;
-};
+import logo from "../public/logo.png";
+
+const abi = [
+  {
+    inputs: [],
+    name: "acquireReward",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_premint",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "REWARDS_AMOUNT",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "REWARDS_TIMEOUT",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "token",
+    outputs: [
+      {
+        internalType: "contract NFTDToken",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+const address = "0xDD2D6aEfD63c095BE1DF4F1A6cB0F25D5F8F04F9";
 
 const Navbar = () => {
   const handleFavoritesClick = () => {
@@ -18,12 +79,11 @@ const Navbar = () => {
       <div className="logo">
         <Link href="/">
           <Image
-            src="/logo.png"
+            src={logo}
             alt="Logo"
             width={300}
             height={50}
-            style={{ width: "auto", height: "auto" }}
-            priority
+            className="cursor-pointer"
           />
         </Link>
       </div>
@@ -32,11 +92,18 @@ const Navbar = () => {
         <motion.button
           className="rounded-xl py-2 px-4 bg-blue-500 text-white font-bold mr-4"
           style={{ minWidth: "120px", textAlign: "center" }}
-          onClick={handleFavoritesClick}
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.9 }}
         >
           My favorites
+        </motion.button>
+        <motion.button
+          className="rounded-xl py-2 px-4 bg-blue-500 text-white font-bold mr-4"
+          style={{ minWidth: "120px", textAlign: "center" }}
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          Daily Quest
         </motion.button>
         <ConnectButton />
       </div>
