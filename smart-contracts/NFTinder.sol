9e8eea88013a1b1e669e1dd2930d801ddfc74155
ksenia-ekshova <ksenia.ekshova.gmail.com>
2023-06-10 23:06:02 +0600

    Token deleted, added  NFT collection, days count

--- smart-contracts/NFTinder.sol
+++ smart-contracts/NFTinder.sol
@@ -1,30 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-contract NFTDToken is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("NFTinder", "NFTD") {}
-
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
-    }
-
-    function decimals() public view virtual override returns (uint8) {
-        return 0;
-    }
-
-}
 
 contract NFTinderCollection is ERC721Enumerable, ERC721Burnable, Ownable {
     using Strings for uint256;
 
-    uint256          maxSupply;
-    string           baseURI;// = "ipfs://QmbeT7zTp5nFbg4BzZJMKT1Ck71MxxXix4oBTr7GzRWKL6/"; donuts collection
+    uint256 public         maxSupply;
+    string public          baseURI;// = "ipfs://QmbeT7zTp5nFbg4BzZJMKT1Ck71MxxXix4oBTr7GzRWKL6/"; donuts collection
 
     constructor(string memory _baseURI, uint _maxSupply) ERC721("NFTinder Collection", "NFTD") {
         baseURI = _baseURI;
@@ -74,42 +60,38 @@ contract NFTinderCollection is ERC721Enumerable, ERC721Burnable, Ownable {
 // import "./NFTinderCollection.sol"; - перенести в другие файлы? деплоить отдельно?
 
 contract NFTDProtocol {
-    NFTDToken public          token;
     NFTinderCollection public nft;
     uint                      nftId = 0;
     mapping(address => mapping(string => uint)) userStats;
 
-    //uint public constant    REWARDS_TIMEOUT = 1 seconds; - for test
-    uint public constant    REWARDS_TIMEOUT = 1 days;
-    uint public constant    REWARDS_AMOUNT  = 10;
-    uint public constant DAYS_TO_NFT_REWARD = 5;
+    uint public constant    REWARDS_TIMEOUT = 1 seconds; //- for test
+    //uint public constant    REWARDS_TIMEOUT = 1 days;
+    uint public constant    DAYS_TO_NFT_REWARD = 5;
 
-    event RewardToken (address indexed user, uint amount, string message);
+    event DailyQuest (address indexed user, uint daysLeft, string message);
     event RewardNFT (address indexed user, string uri, string message);
 
-    constructor(uint _premint, string memory _baseURI, uint _maxSupply) {
-        token = new NFTDToken();
+    constructor(string memory _baseURI, uint _maxSupply) {
         nft = new NFTinderCollection(_baseURI,_maxSupply);
-        token.mint(address(this), _premint);
     }
 
     function acquireReward () external returns (uint) {
         require(block.timestamp - userStats[msg.sender]["lastReward"] >= REWARDS_TIMEOUT, "You can receive the reward only once a day");
-        require(token.balanceOf(address(this)) >= REWARDS_AMOUNT, "Not enough tokens in the reserve");
 
         if (userStats[msg.sender]["dayStreak"] < DAYS_TO_NFT_REWARD) {
             userStats[msg.sender]["lastReward"] = block.timestamp;
-            token.transfer(msg.sender, REWARDS_AMOUNT);
             userStats[msg.sender]["dayStreak"]++;
-            emit RewardToken(msg.sender, REWARDS_AMOUNT, "You received a reward for activity");
-            return (DAYS_TO_NFT_REWARD - userStats[msg.sender]["dayStreak"]);
+            uint256 daysLeft = DAYS_TO_NFT_REWARD - userStats[msg.sender]["dayStreak"];
+            emit DailyQuest(msg.sender, daysLeft, " days left to receive the reward");
+            return daysLeft;
         } else {
+            require(nftId < nft.maxSupply(), "All NFTs are minted");
             userStats[msg.sender]["lastReward"] = block.timestamp;
             nft.safeMint(msg.sender, nftId);
+            string memory uri = nft.tokenURI(nftId);
             nftId++;
             userStats[msg.sender]["dayStreak"] = 0;
-
-            emit RewardToken(msg.sender, REWARDS_AMOUNT * DAYS_TO_NFT_REWARD, "You received a reward for activity");
+            emit RewardNFT(msg.sender, uri, "You received a reward for activity");
             return userStats[msg.sender]["dayStreak"];
         }
     }
@@ -125,11 +107,6 @@ contract NFTDProtocol {
 
     function getNFTId() external view returns (uint) {
         return nftId;
-    }
-
-    //get token balance of user
-    function getTokenBalance(address _user) external view returns (uint) {
-        return token.balanceOf(_user);
     }
 
     //get nft balance of user
